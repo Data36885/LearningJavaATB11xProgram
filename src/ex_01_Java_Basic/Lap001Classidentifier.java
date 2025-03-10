d327ba1c2753bd93ed182e31db88e6cb711cf096
hp <data36885@gmail.com>
2025-03-10 23:04:21 +0530

    10 mar 2025

---
+++ src/ex_01_Java_Basic/Lap001Classidentifier.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lap001Classidentifier {
+    public static void main(String[] args) {
+        System.out.println("Key identifier");
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2+2);
+    }
+}
